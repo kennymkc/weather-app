648a0d8e63b4718c475b37cd5b6532b905c4e435
Kennie <Kenneth Chan>
2023-02-27 16:05:52 -0500

    connect to GeoDB api

---
+++ src/api.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a105636981msh6c55d6d6460c831p104abfjsn51b0005250e9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from '../api';
 
 const Search = ({onSearchChange}) => {
 
   const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions
+    )
+	  .then(response => response.json())
+	  .then(response => console.log(response))
+	  .catch(err => console.error(err));
+  }
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
@@ -16,6 +26,7 @@ const Search = ({onSearchChange}) => {
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   )
 }
