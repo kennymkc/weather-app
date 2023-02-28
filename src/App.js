7e2e05bae0fa480e8cf7834719c58662a006e617
Kennie <Kenneth Chan>
2023-02-28 15:44:14 -0500

    create placeholder for current weather

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Search from './components/search';
+import CurrentWeather from './components/current-weather';
 import './App.css';
 
 function App() {
@@ -6,10 +7,11 @@ function App() {
   const handleOnSearchChange = (searchData) => {
     console.log(searchData);
   }
-  
+
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
