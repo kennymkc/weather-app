b571d5153d3f655ca3b1ea3b693b3aa2e89fb7fc
Kennie <Kenneth Chan>
2023-03-01 15:16:31 -0500

    connect to openweather api to replace static data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import Search from './components/search';
 import CurrentWeather from './components/current-weather';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (res) => {
+        const weatherResponse = await res[0].json();
+        const forecastResponse = await res[1].json();
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+
   }
+
+  console.log("current weather: ", currentWeather);
+  console.log("forecast: ", forecast)
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
